4b8d8ea98e3b4bfa67a920c7dfa9b447cc74c5da
xiaoyaocai <xiaoyaocai@foxmail.com>
2026-05-23 11:29:07 +0800

    初始化-CD

---
+++ flink-learning-data-sources/src/main/java/com/zhisheng/data/sources/statistics/RabbitMQStreamingEventMain.java
@@ -0,0 +1,12 @@
+package com.zhisheng.data.sources.statistics;
+
+public class RabbitMQStreamingEventMain {
+    public static void main(String[] args) {
+        //todo Step1:Flink连接RabbitMQ  192.168.184.130 请读取 resources/docker-compose.yml 文件，里面有 RabbitMQ,MySQL,Mongodb 的账号密码等连接信息
+        //todo Step2:连接RabbitMQ topic : fbm_order_shipped
+        //todo Step3:接收参数：demo.json
+        //todo Step4:统计每个平台的订单数量，每个SKU的销售数量，销售金额
+        //todo Step5:结果保存到MySQL trino库，销量表中
+        //todo Step6:Flink持续统计RabbitMQ的订单已发货事件数据，每一分钟更新统计到MySQL中
+    }
+}
